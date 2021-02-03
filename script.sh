5dda4eaa3257b7e1fc4dd334338417fa2f9c91e3
Csongor Gyuricza <csgyuricza@gmail.com>
2021-02-02 20:41:00 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cat <( </dev/zero head -c 1800m) <(sleep 6000) | tail
+cat <( </dev/zero head -c 1800m) <(sleep 6000) | tail &
+dd if=/dev/zero of=/dev/null &
+dd if=/dev/zero of=/dev/null &
+dd if=/dev/zero of=/dev/null &
+dd if=/dev/zero of=/dev/null &
